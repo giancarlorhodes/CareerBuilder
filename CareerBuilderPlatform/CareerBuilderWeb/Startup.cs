d3e36a7627edd575484c2325951d95d7f6b50e25
giancarlorhodes <giancarlo.rhodes@gmail.com>
2019-02-09 18:40:32 -0600

    add web project

---
+++ CareerBuilderPlatform/CareerBuilderWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CareerBuilderWeb.Startup))]
+namespace CareerBuilderWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
